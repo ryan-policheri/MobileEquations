8d282cef59cf09614caee4e415b2b173fc8377f4
Ryan Policheri <Ryan-Poli90@outlook.com>
2021-11-08 20:24:08 -0600

    log errors

--- WebApi/MobileEquations.WebApi/MobileEquations.WebApi/Controllers/EquationsController.cs
+++ WebApi/MobileEquations.WebApi/MobileEquations.WebApi/Controllers/EquationsController.cs
@@ -38,22 +38,30 @@ namespace MobileEquations.WebApi.Controllers
         [HttpPost]
         public Equation Solve([ModelBinder(BinderType = typeof(JsonModelBinder))] Equation equation, IFormFile photo)
         {
-            equation.Photo = photo.ToInMemoryFile();
-            string requestDirectory = CreateRequestDirectory();
-            string photoPath = SystemFunctions.CombineDirectoryComponents(requestDirectory, equation.Photo.FileName);
-            string inputFile = SystemFunctions.CombineDirectoryComponents(requestDirectory, "Input.json");
-            string outputFile = SystemFunctions.CombineDirectoryComponents(requestDirectory, "Output.json");
+            try
+            {
+                equation.Photo = photo.ToInMemoryFile();
+                string requestDirectory = CreateRequestDirectory();
+                string photoPath = SystemFunctions.CombineDirectoryComponents(requestDirectory, equation.Photo.FileName);
+                string inputFile = SystemFunctions.CombineDirectoryComponents(requestDirectory, "Input.json");
+                string outputFile = SystemFunctions.CombineDirectoryComponents(requestDirectory, "Output.json");
 
-            SystemFunctions.CreateFile(photoPath, equation.Photo.Bytes);
-            SystemFunctions.CreateFile(inputFile, equation.ToJson());
+                SystemFunctions.CreateFile(photoPath, equation.Photo.Bytes);
+                SystemFunctions.CreateFile(inputFile, equation.ToJson());
 
-            string command = $"\"{_config.EquationSolverScript}\" \"{inputFile}\" \"{outputFile}\"";
-            _logger.LogInformation($"Calling \"{_config.PythonExecutable}\" with the following command: {command}");
-            SystemFunctions.RunCustomProcess($"\"{_config.PythonExecutable}\"", command);
-            string output = SystemFunctions.ReadAllText(outputFile);
-            equation.ProcessedEquation = output.ConvertJsonToObject<ProcessedEquation>(JsonSerializationOptions.CaseInsensitive);
+                string command = $"{_config.EquationSolverScript.Quotify()} {inputFile.Quotify()} {outputFile.Quotify()}";
+                _logger.LogInformation($"Calling {_config.PythonExecutable.Quotify()} with the following command: {command}");
+                SystemFunctions.RunCustomProcess($"{_config.PythonExecutable.Quotify()}", command);
+                string output = SystemFunctions.ReadAllText(outputFile);
+                equation.ProcessedEquation = output.ConvertJsonToObject<ProcessedEquation>(JsonSerializationOptions.CaseInsensitive);
 
-            return equation;
+                return equation;
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, exception.Message);
+                throw;
+            }
         }
 
         private string CreateRequestDirectory()
