0b880fdac09ca99b7d5a7db43b54f956af326974
LvnL <levin-leesemann@uiowa.edu>
2021-12-07 01:02:27 -0600

    Add basic thresholding

--- EquationSolver/image_preprocessor.py
+++ EquationSolver/image_preprocessor.py
@@ -1,23 +1,10 @@
-import skimage.io
-import skimage.transform
-import skimage.util
+from skimage.io import imread
+from skimage.filters import threshold_local
 
 
 def image_preprocessor(image_path):
-    print("Preprocessing image \"{}\"...".format(image_path))
-    
-    image = "preprocessed image"
-    
-    return image
+    image = imread(image_path, as_gray=True)
+    threshold = threshold_local(image, 101, offset=0.02)
+    image_threshold = image > threshold
 
-    image = skimage.io.imread(image_path, as_gray=True)
-    image = process_image(image)
-
-
-def process_image(image):
-    image = skimage.util.invert(image)
-    image = skimage.transform.resize(image, (28, 28))
-    image = image.reshape(28, 28, 1)
-    image = image.astype("float32")
-
-    return image
+    return image_threshold
