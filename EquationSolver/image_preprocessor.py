8613a5fa2ba034f350209e081e982bf5603b8273
LvnL <levin-leesemann@uiowa.edu>
2021-12-08 10:30:41 -0600

    Add text color evaluation and thresholding

--- EquationSolver/image_preprocessor.py
+++ EquationSolver/image_preprocessor.py
@@ -1,10 +1,32 @@
-from skimage.io import imread
-from skimage.filters import threshold_local
+import sys
+
+from utilities.image.display import show
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 
 
 def image_preprocessor(image_path):
-    image = imread(image_path, as_gray=True)
-    threshold = threshold_local(image, 101, offset=0.02)
-    image_threshold = image > threshold
+    image = cv.imread(image_path, 0)
+    blur = cv.medianBlur(image, 5)
 
-    return image_threshold
+    # Black: 0, White: 255
+    median = np.median(blur)
+    maximum = np.max(blur)
+    minimum = np.min(blur)
+
+    text_color = "white" if abs(median - maximum) > abs(median - minimum) else "black"
+
+    if text_color == "white":
+        blur = np.bitwise_not(blur)
+        
+    threshold = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 31, 5)
+        
+    return threshold
+
+
+if __name__ == "__main__":
+    output = image_preprocessor(sys.argv[1])
+    show(output)
